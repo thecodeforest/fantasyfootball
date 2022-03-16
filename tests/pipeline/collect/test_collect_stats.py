abe7444420eaad2c27d698892f82cb199d3c650a
Mark LeBoeuf <mark.leboeuf@nike.com>
2022-03-16 12:43:37 -0700

    test: add test for collecting player offense statistics

---
+++ tests/pipeline/collect/test_collect_stats.py
@@ -0,0 +1,193 @@
+import pytest
+
+from fantasyfootball.pipeline.collect.collect_stats import (
+    clean_player_name,
+    create_abbr_name_combo,
+    create_player_id,
+    first_name_is_abbr
+)
+
+
+@pytest.mark.parametrize(
+    ("first_name", "expected"),
+    (
+        ("D.J. Green", True),
+        ("Tom Brady", False),
+        ("D.K. Metcalf", True),
+        ("DJ Chark", False),
+        ("D'Wayne Eskridge", False),
+        ("Amon-Ra St. Brown", False),
+    ),
+)
+def test_first_name_is_abbr(first_name, expected):
+    assert first_name_is_abbr(first_name) == expected
+
+
+@pytest.mark.parametrize(
+    ("name", "_type", "expected"),
+    (
+        ("Tom", "first", "Tom"),
+        ("Brady", "last", "Brady"),
+        ("DJ", "first", "DJ"),
+        ("Chark", "last", "Chark"),
+        ("D'Wayne", "first", "DWayne"),
+        ("Peoples-Jones", "last", "PeoplesJones"),
+    ),
+)
+def test_clean_name(name, _type, expected):
+    assert clean_player_name(name, _type) == expected
+
+
+def test_valid_clean_name():
+    with pytest.raises(ValueError):
+        clean_player_name("John", "middle")
+
+
+def test_create_abbr_name_combo():
+    first_name = "D.J."
+    expected = ("DJ", "D.", "D")
+    assert create_abbr_name_combo(first_name) == expected
+
+
+def test_create_player_id_no_abbr():
+    first_name = "Tom"
+    last_name = "Brady"
+    expected = [
+        "BradTo00",
+        "BradTo01",
+        "BradTo02",
+        "BradTo03",
+        "BradTo04",
+        "BradTo05",
+        "BradTo06",
+        "BradTo07",
+        "BradTo08",
+        "BradTo09",
+        "BradTo10",
+        "BradTo11",
+        "BradTo12",
+        "BradTo13",
+        "BradTo14",
+        "BradTo15",
+        "BradTo16",
+        "BradTo17",
+        "BradTo18",
+        "BradTo19",
+    ]
+    assert create_player_id(first_name, last_name) == expected
+
+
+def test_create_player_id_abbr():
+    first_name = "D.K."
+    last_name = "Metcalf"
+    expected = [
+        "MetcDK00",
+        "MetcD.00",
+        "MetcD00",
+        "MetcDK01",
+        "MetcD.01",
+        "MetcD01",
+        "MetcDK02",
+        "MetcD.02",
+        "MetcD02",
+        "MetcDK03",
+        "MetcD.03",
+        "MetcD03",
+        "MetcDK04",
+        "MetcD.04",
+        "MetcD04",
+        "MetcDK05",
+        "MetcD.05",
+        "MetcD05",
+        "MetcDK06",
+        "MetcD.06",
+        "MetcD06",
+        "MetcDK07",
+        "MetcD.07",
+        "MetcD07",
+        "MetcDK08",
+        "MetcD.08",
+        "MetcD08",
+        "MetcDK09",
+        "MetcD.09",
+        "MetcD09",
+        "MetcDK10",
+        "MetcD.10",
+        "MetcD10",
+        "MetcDK11",
+        "MetcD.11",
+        "MetcD11",
+        "MetcDK12",
+        "MetcD.12",
+        "MetcD12",
+        "MetcDK13",
+        "MetcD.13",
+        "MetcD13",
+        "MetcDK14",
+        "MetcD.14",
+        "MetcD14",
+        "MetcDK15",
+        "MetcD.15",
+        "MetcD15",
+        "MetcDK16",
+        "MetcD.16",
+        "MetcD16",
+        "MetcDK17",
+        "MetcD.17",
+        "MetcD17",
+        "MetcDK18",
+        "MetcD.18",
+        "MetcD18",
+        "MetcDK19",
+        "MetcD.19",
+        "MetcD19",
+    ]
+    assert create_player_id(first_name, last_name) == expected
+
+
+def test_create_player_id_apostrophe():
+    first_name = "James"
+    last_name = "O'Shaughnessys"
+    expected = [
+        "O'ShJa00",
+        "OShaJa00",
+        "O'ShJa01",
+        "OShaJa01",
+        "O'ShJa02",
+        "OShaJa02",
+        "O'ShJa03",
+        "OShaJa03",
+        "O'ShJa04",
+        "OShaJa04",
+        "O'ShJa05",
+        "OShaJa05",
+        "O'ShJa06",
+        "OShaJa06",
+        "O'ShJa07",
+        "OShaJa07",
+        "O'ShJa08",
+        "OShaJa08",
+        "O'ShJa09",
+        "OShaJa09",
+        "O'ShJa10",
+        "OShaJa10",
+        "O'ShJa11",
+        "OShaJa11",
+        "O'ShJa12",
+        "OShaJa12",
+        "O'ShJa13",
+        "OShaJa13",
+        "O'ShJa14",
+        "OShaJa14",
+        "O'ShJa15",
+        "OShaJa15",
+        "O'ShJa16",
+        "OShaJa16",
+        "O'ShJa17",
+        "OShaJa17",
+        "O'ShJa18",
+        "OShaJa18",
+        "O'ShJa19",
+        "OShaJa19",
+    ]
+    assert create_player_id(first_name, last_name) == expected

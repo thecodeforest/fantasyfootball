64760f40d6ba7232609e70822f6155cc96108330
Mark LeBoeuf <mark.leboeuf@nike.com>
2022-04-03 13:06:36 -0700

    feat: add config file for root directory and key urls

---
+++ src/fantasyfootball/config.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+root_dir = Path(__file__).parent
+stats_url = "https://www.pro-football-reference.com"
+betting_url = (
+    "https://www.sportsbookreviewsonline.com/scoresoddsarchives/nfl/nfl%20odds%2020"
+)

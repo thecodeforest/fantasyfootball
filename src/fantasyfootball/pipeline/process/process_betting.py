2ae3f279d7e42047f6004f497280abd52bfdd5be
Mark LeBoeuf <mark.leboeuf@nike.com>
2022-05-16 14:39:24 -0700

    feat: add imputation when point projections are missing

--- src/fantasyfootball/pipeline/process/process_betting.py
+++ src/fantasyfootball/pipeline/process/process_betting.py
@@ -1,7 +1,10 @@
 from itertools import chain
+from typing import List
 
+import numpy as np
 import pandas as pd
 import pandas_flavor as pf
+from janitor import clean_names
 
 from fantasyfootball.config import root_dir
 from fantasyfootball.pipeline.utils import (
@@ -10,8 +13,6 @@ from fantasyfootball.pipeline.utils import (
     read_ff_csv,
     retrieve_team_abbreviation,
 )
-
-from janitor import clean_names  # noqa  # isort:skip
 
 
 def clean_game_date(season_year: int, date: str) -> str:
@@ -146,6 +147,62 @@ def process_betting(df: pd.DataFrame, season_year: int) -> pd.DataFrame:
     return process_betting_df
 
 
+def impute_missing_projections(
+    df: pd.DataFrame,
+    calendar_df: pd.DataFrame,
+    keys: List[str] = ["date", "season_year", "team", "opp"],
+    default_point_projection: int = 25,
+) -> pd.DataFrame:
+    """Impute missing point projections for each team in each game.
+       Some dates for betting are incorrect. In these instances,
+       the point projections are imputed with the average point projection
+       for all previous games played in the same season.
+
+    Args:
+        df (pd.DataFrame): Dataframe with point projections for each team in each game.
+        calendar_df (pd.DataFrame): Dataframe with dates for each game.
+        keys (List[str], optional): Columns to join the point projections and calendar.
+            Defaults to ["date", "season_year", "team", "opp"].
+        default_point_projection (int, optional): If a team has no
+            previous games (i.e., it's the first
+            game of the season), the point projection is imputed with this value.
+            Defaults to 25.
+
+    Returns:
+        pd.DataFrame: Dataframe with imputed point projections for each team in
+            each game. There should be no NA values for the point projections.
+    """
+    # identify games without a projection value
+    missing_projection_df = pd.merge(calendar_df, df, on=keys, how="left")
+    if not missing_projection_df["projected_off_pts"].isnull().sum():
+        return pd.DataFrame()
+    missing_projection_df = missing_projection_df[
+        pd.isna(missing_projection_df["projected_off_pts"])
+    ]
+    imputed_projections_lst = list()
+    for row in missing_projection_df.itertuples(index=False):
+        date, season_year, team, opp = row.date, row.season_year, row.team, row.opp
+        # filter prior projections and averge together for imputed projection
+        prior_week_projections_df = df[(df["date"] < date) & (df["team"] == team)]
+        if prior_week_projections_df.shape[0] == 0:
+            print(
+                "No historical data to calculate projection."
+                f"Using default projection of {default_point_projection}"
+            )
+            avg_week_projections = default_point_projection
+        else:
+            avg_week_projections = round(
+                np.average(prior_week_projections_df["projected_off_pts"])
+            )
+        imputed_projections_lst.append(
+            [date, season_year, team, opp, avg_week_projections]
+        )
+    imputed_projections_df = pd.DataFrame(
+        imputed_projections_lst, columns=keys + ["projected_off_pts"]
+    )
+    return imputed_projections_df
+
+
 if __name__ == "__main__":
     args = read_args()
     dir_type, data_type = get_module_purpose(module_path=__file__)
@@ -153,6 +210,15 @@ if __name__ == "__main__":
         root_dir / "datasets" / "season" / str(args.season_year) / "raw" / data_type
     )
     clean_betting_df = read_ff_csv(raw_data_dir)
+    calendar_df = pd.read_csv(
+        root_dir
+        / "datasets"
+        / "season"
+        / str(args.season_year)
+        / "processed"
+        / "calendar"
+        / "calendar.csv"
+    )
     clean_betting_df = (
         clean_betting_df.clean_names()
         .clean_games_date(args.season_year)
@@ -160,4 +226,7 @@ if __name__ == "__main__":
         .add_team_abbreviation()
         .process_betting(args.season_year)
     )
+    imputed_projection_df = impute_missing_projections(clean_betting_df, calendar_df)
+    if not imputed_projection_df.empty:
+        clean_betting_df = pd.concat([clean_betting_df, imputed_projection_df])
     clean_betting_df.write_ff_csv(root_dir, args.season_year, dir_type, data_type)

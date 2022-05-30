cd415b01b489084e37d58ee25f2193fe60c5b9b0
Mark LeBoeuf <mark.leboeuf@nike.com>
2022-05-30 16:43:33 -0700

    refactor: remove websites for data pipeline

--- src/fantasyfootball/config.py
+++ src/fantasyfootball/config.py
@@ -1,11 +1,6 @@
 from pathlib import Path
 
 root_dir = Path(__file__).parent
-stats_url = "https://www.pro-football-reference.com"
-betting_url = (
-    "https://www.sportsbookreviewsonline.com/scoresoddsarchives/nfl/nfl%20odds%2020"
-)
-injury_url = "https://www.footballdb.com/transactions/injuries.html"
 data_sources = {
     "calendar": {
         "keys": ["team", "season_year"],

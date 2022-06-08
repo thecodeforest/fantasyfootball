01885b75dc39c7aa04ea6645ea8dd3981a613592
Mark LeBoeuf <mark.leboeuf@nike.com>
2022-06-08 10:08:43 -0700

    fix: update pipeline path for collecting calendar

--- pipeline/collect/collect_calendar.py
+++ pipeline/collect/collect_calendar.py
@@ -4,38 +4,35 @@ from pathlib import Path
 import pandas as pd
 
 sys.path.append(str(Path.cwd()))
-print("Here are paths:")
-for path in sys.path:
-    print(path)
 from pipeline.pipeline_config import root_dir, stats_url  # noqa: E402
 
-# from pipeline.utils import get_module_purpose, read_args, write_ff_csv  # noqa: E402
+from pipeline.utils import get_module_purpose, read_args, write_ff_csv  # noqa: E402
 
 
-# def collect_calendar(calendar_url: str) -> pd.DataFrame:
-#     """Collects the calendar data for a complete season. Includes
-#        data on date, time, winner, loser, box-score, total points
-#        for each team.
+def collect_calendar(calendar_url: str) -> pd.DataFrame:
+    """Collects the calendar data for a complete season. Includes
+       data on date, time, winner, loser, box-score, total points
+       for each team.
 
-#     Args:
-#         calendar_url (str): The url to scrape the calendar data from.
+    Args:
+        calendar_url (str): The url to scrape the calendar data from.
 
-#     Returns:
-#         pd.DataFrame: The calendar data.
-#     """
-#     calendar_df = pd.read_html(calendar_url)[0]
-#     return calendar_df
+    Returns:
+        pd.DataFrame: The calendar data.
+    """
+    calendar_df = pd.read_html(calendar_url)[0]
+    return calendar_df
 
 
-# if __name__ == "__main__":
-#     args = read_args()
-#     dir_type, data_type = get_module_purpose(module_path=__file__)
-#     calendar_url = f"{stats_url}/years/{args.season_year}/games.htm"
-#     calendar_raw = collect_calendar(calendar_url=calendar_url)
-#     print(root_dir)
-#     calendar_raw.write_ff_csv(
-#         root_dir=root_dir,
-#         season_year=args.season_year,
-#         dir_type=dir_type,
-#         data_type=data_type,
-#     )
+if __name__ == "__main__":
+    args = read_args()
+    dir_type, data_type = get_module_purpose(module_path=__file__)
+    calendar_url = f"{stats_url}/years/{args.season_year}/games.htm"
+    calendar_raw = collect_calendar(calendar_url=calendar_url)
+    print(root_dir)
+    calendar_raw.write_ff_csv(
+        root_dir=root_dir,
+        season_year=args.season_year,
+        dir_type=dir_type,
+        data_type=data_type,
+    )

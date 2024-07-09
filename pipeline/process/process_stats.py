2e0fe9b350409d3df065a8213ab2b5a7848479ad
Mark LeBoeuf <markleboeuf2015@u.northwestern.edu>
2024-07-09 06:08:18 -0700

    add processing step to pipeline for stats

--- pipeline/process/process_stats.py
+++ pipeline/process/process_stats.py
@@ -36,31 +36,32 @@ def create_stats_id(row):
 
 def process_stats():
     logger.info('Processing stats data')
-    dp_root = Path(os.getenv('DATA_PIPELINE_ROOT', Path.cwd())).parent.parent  
+    dp_root = Path(os.getenv('DATA_PIPELINE_ROOT', Path.cwd()))
     logger.info(f"dp root is {dp_root}")
-    read_path = dp_root / "data" / "raw" / "stats"
+    # /home/runner/work/data/raw/stats
+    read_path = dp_root.parent.parent / "data" / "raw" / "stats"
     logger.info(f"read path is {read_path}")
-    # write_path = dp_root / "data" / "processed" / "stats"   
-    # input_raw_stats_files = [file for file in read_path.glob('**/*.csv') if file.is_file()]  
-    # for file_path in input_raw_stats_files:
-    #     position, year, week, fname = str(file_path).split("/")[-4:]
-    #     df = pd.read_csv(file_path)
-    #     df = df.rename(columns=str.lower)
-    #     df = df.drop(columns=["rank", "fp/g", "fantpt", "fg%", "y/att", 'cm%', 'y/rsh', 'y/rec', 'g'], errors='ignore')
-    #     df = rename_columns(df, PLAYER_STATS_COLUMN_MAPPING)
-    #     df = df.fillna(0)
-    #     df['position'] = position
-    #     df['position'] = df['position'].str.upper()
-    #     df['week'] = week
-    #     df['season'] = year   
-    #     df.loc[df['position'] == 'PK', 'position'] = 'K'
-    #     df['team']  = df['name'].apply(lambda x: x.split(" ")[-1])
-    #     df['name'] = df.apply(lambda row: row['name'].replace(row['team'], ''), axis=1)
-    #     df['name'] = df['name'].str.strip()
-    #     df['stats_id'] = df.apply(create_stats_id, axis=1)
-    #     df['draft_id'] = df.apply(create_fantasy_draft_id, axis=1)
-    #     Path(write_path / position).mkdir(parents=True, exist_ok=True)
-    #     df.to_csv(Path(write_path) / position / fname, index=False)
+    write_path = dp_root.parent.parent / "data" / "processed" / "stats"   
+    input_raw_stats_files = [file for file in read_path.glob('**/*.csv') if file.is_file()]  
+    for file_path in input_raw_stats_files:
+        position, year, week, fname = str(file_path).split("/")[-4:]
+        df = pd.read_csv(file_path)
+        df = df.rename(columns=str.lower)
+        df = df.drop(columns=["rank", "fp/g", "fantpt", "fg%", "y/att", 'cm%', 'y/rsh', 'y/rec', 'g'], errors='ignore')
+        df = rename_columns(df, PLAYER_STATS_COLUMN_MAPPING)
+        df = df.fillna(0)
+        df['position'] = position
+        df['position'] = df['position'].str.upper()
+        df['week'] = week
+        df['season'] = year   
+        df.loc[df['position'] == 'PK', 'position'] = 'K'
+        df['team']  = df['name'].apply(lambda x: x.split(" ")[-1])
+        df['name'] = df.apply(lambda row: row['name'].replace(row['team'], ''), axis=1)
+        df['name'] = df['name'].str.strip()
+        df['stats_id'] = df.apply(create_stats_id, axis=1)
+        df['draft_id'] = df.apply(create_fantasy_draft_id, axis=1)
+        Path(write_path / position).mkdir(parents=True, exist_ok=True)
+        df.to_csv(Path(write_path) / position / fname, index=False)
     logger.info('Stats data processing complete')
 
 if __name__ == "__main__":
